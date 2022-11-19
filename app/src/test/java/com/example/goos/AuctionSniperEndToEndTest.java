69de94d40be71f6fbfe159641ab6abb83a4fbd6c
ita-jp <tn00gm@gmail.com>
2022-11-19 21:09:19 +0900

    p.90 第10章 動くスケルトン - 一番最初のテスト

---
+++ app/src/test/java/com/example/goos/AuctionSniperEndToEndTest.java
@@ -0,0 +1,29 @@
+package com.example.goos;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class AuctionSniperEndToEndTest {
+
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() {
+        auction.startSellingItem();
+        application.startBiddingIn(acution);
+        auction.hasRecievedJoinRequestFromSniper();
+        auction.announceClosed();
+        application.showsSniperHasLostAuction();
+    }
+
+    @AfterEach
+    public void stopAuction() {
+        auction.stop();
+    }
+
+    @AfterEach
+    public void stopApplication() {
+        application.stop();
+    }
+}
