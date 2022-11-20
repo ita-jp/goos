946a5225031420d17292ee507f6e41eef77be5b4
ita-jp <tn00gm@gmail.com>
2022-11-20 14:38:42 +0900

    第11章 最初のテストを通す - ニセのオークション (1)
    はじめのコードブロック

--- app/src/test/java/com/example/goos/FakeAuctionServer.java
+++ app/src/test/java/com/example/goos/FakeAuctionServer.java
@@ -1,4 +1,34 @@
 package com.example.goos;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
+import static java.lang.String.format;
+
 public class FakeAuctionServer {
+
+    public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+    public static final String AUCTION_RESOURCE = "Auction";
+    public static final String XMPP_HOSTNAME = "localhost";
+    private static final String AUCTION_PASSWORD = "auction";
+
+    private final String itemId;
+    private final XMPPConnection connection;
+    private Chat currentChat;
+
+    public FakeAuctionServer(String itemId) {
+        this.itemId = itemId;
+        this.connection = new XMPPConnection(XMPP_HOSTNAME);
+    }
+
+    public void startSellingItem() throws XMPPException {
+        connection.connect();
+        connection.login(format(ITEM_ID_AS_LOGIN, itemId), AUCTION_PASSWORD, AUCTION_RESOURCE);
+        connection.getChatManager().addChatListener((chat, createdLocally) -> currentChat = chat);
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
 }
