3abe1d7690ecedd65ee62096695b269ff1de5d66
ita-jp <tn00gm@gmail.com>
2022-11-21 23:04:33 +0900

    p125. 第12章：入札を準備する - 価格メッセージを解析する

--- app/src/main/java/com/example/goos/auctionsniper/AuctionMessageTranslator.java
+++ app/src/main/java/com/example/goos/auctionsniper/AuctionMessageTranslator.java
@@ -5,6 +5,10 @@ import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.packet.Message;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class AuctionMessageTranslator implements MessageListener {
 
     private AuctionEventListener listener;
@@ -15,6 +19,20 @@ public class AuctionMessageTranslator implements MessageListener {
 
     @Override
     public void processMessage(Chat chat, Message message) {
-        listener.auctionClosed();
+        var event = unpackEventFrom(message);
+        var type = event.get("Event");
+        if ("CLOSE".equals(type)) {
+            listener.auctionClosed();
+        } else if ("PRICE".equals(type)) {
+            var price = Integer.parseInt(event.get("CurrentPrice"));
+            var increment = Integer.parseInt(event.get("Increment"));
+            listener.currentPrice(price, increment);
+        }
+    }
+
+    private Map<String, String> unpackEventFrom(Message message) {
+        return Arrays.stream(message.getBody().split(";"))
+                .map(kv -> kv.split(":"))
+                .collect(Collectors.toMap(kv -> kv[0].trim(), kv -> kv[1].trim()));
     }
 }
